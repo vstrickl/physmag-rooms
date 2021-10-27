e17502b1fa89c61c8474e2c05916538b4e5c658b
Vonique Stricklen <vstrickl.git@gmail.com>
2021-10-26 21:28:37 -0700

    Updated Style with Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import FullCalendar from '@fullcalendar/react' // must go before plugins
 // a plugin!
 import timeGridPlugin from '@fullcalendar/timegrid'
 import dayGridPlugin from '@fullcalendar/daygrid'
-import googleCalendarPlugin from '@fullcalendar/google-calendar';
+import googleCalendarPlugin from '@fullcalendar/google-calendar'
 
 const API_KEY = process.env.REACT_APP_API_KEY
 const CALENDAR_ID = process.env.REACT_APP_CALENDAR_ID
@@ -12,16 +12,20 @@ const CALENDAR_ID = process.env.REACT_APP_CALENDAR_ID
 export default class App extends React.Component {
   render() {
     return (
-    <>
-      <h3>Physique Magnifique Calendar</h3>
-      <h2>Yoga Studio</h2>
+    <div className="container">
+      <div className="row mt-3 mb-5">
+        <h4>Physique Magnifique Calendar</h4>
+        <h2>Yoga Studio</h2>
+      </div>
+      
       <FullCalendar
         plugins={[ timeGridPlugin, dayGridPlugin, googleCalendarPlugin ]}
         googleCalendarApiKey={API_KEY}
         events={{
           googleCalendarId: CALENDAR_ID
         }}
-        initialView="timeGridWeek"
+        initialView='timeGridWeek'
+        themeSystem='standard'
         headerToolbar={{
           start: 'prev', // will normally be on the left. if RTL, will be on the right
           center: 'title',
@@ -29,7 +33,7 @@ export default class App extends React.Component {
         }}
 
       />
-    </>
+    </div>
     )
   }
 }

07e9d9ae843ab1676a5705ed54fa5b930c3334a6
Vonique Stricklen <vstrickl.git@gmail.com>
2021-10-30 00:54:41 -0700

    Updated text and scrollbar style

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import App from './main';
 
 ReactDOM.render(
   <React.StrictMode>
